1b782cfdb23d15ba815148b9590f1254e2d50717
Matthias Mitterreiter <matthias@mitterreiter.de>
2024-08-25 21:23:56 +0700

    contains duplicate

---
+++ src/217_contains_duplicat.py
@@ -0,0 +1,53 @@
+"""
+    217. Contains Duplicate
+    Given an integer array nums, return true
+    if any value appears at least twice in the array,
+    and return false if every element is distinct.
+"""
+
+tests = [
+    ([0], False),
+    ([-1, 10], False),
+    ([1, 2, 3, 1], True),
+    ([1, 2, 3, 0], False),
+    ([1, 1, 1, 3, 3, 4, 3, 2, 4, 2], True),
+]
+
+
+def contains_duplicate_hashing(lst):
+    """
+    Time: O(n)
+    Memo: O(n)
+    """
+    n = len(lst)
+    return n != len(set(lst))
+
+
+def contains_duplicate_naive(lst):
+    """
+    Time: O(n^2)
+    Memo: O(1)
+    """
+    for i in range(len(lst)):
+        for j in range(i + 1, len(lst)):
+            if lst[i] == lst[j]:
+                return True
+    return False
+
+
+def contains_duplicate_sort(lst):
+    """
+    Time: O(n log(n))
+    Memo: O(1)
+    """
+    lst.sort()
+    for i in range(len(lst) - 1):
+        if lst[i] == lst[i + 1]:
+            return True
+    return False
+
+
+if __name__ == "__main__":
+    for lst, exp in tests:
+        act = contains_duplicate_sort(lst)
+        assert act == exp
