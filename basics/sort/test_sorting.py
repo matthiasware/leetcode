23628a139eb2da3963696edeabf11aba0056540c
Matthias Mitterreiter <matthias@mitterreiter.de>
2024-08-27 14:24:36 +0700

    Refactor sorting test

--- basics/sort/test_sorting.py
+++ basics/sort/test_sorting.py
@@ -1,27 +1,22 @@
 import random
-lsts = [
-    [],
-    [1],
-    [1,2],
-    [2,1],
-    [1,2,3],
-    [3,2,1],
-    [1,1,0],
-    [1,5,2,3]
-]
+
+lsts = [[], [1], [1, 2], [2, 1], [1, 2, 3], [3, 2, 1], [1, 1, 0], [1, 5, 2, 3]]
+
 
 def test_partition():
     from quicksort import partition
+
     lsts = [
-        ([1,2,4,5,3], 0, 4, [1,2,3,5,4]),
-        ([0,1], 0, 1, [0, 1]),
-        ([0,3,2], 0, 2, [0,2,3]),
-        ([1,0], 0, 1, [0, 1])
+        ([1, 2, 4, 5, 3], 0, 4, [1, 2, 3, 5, 4]),
+        ([0, 1], 0, 1, [0, 1]),
+        ([0, 3, 2], 0, 2, [0, 2, 3]),
+        ([1, 0], 0, 1, [0, 1]),
     ]
-    for (lst_in, i_start, i_end, lst_exp) in lsts:
+    for lst_in, i_start, i_end, lst_exp in lsts:
         lst_in = lst_in[::]
         partition(lst_in, i_start, i_end)
         assert lst_in == lst_exp
+
 
 def test_insertion_sort():
     from quicksort import insertion_sort
@@ -36,6 +31,7 @@ def test_insertion_sort():
 
 def test_quicksort():
     from quicksort import quick_sort
+
     for act in lsts:
         print(act)
         lst = act[::]
@@ -59,8 +55,8 @@ def test_merge_sort():
     for lst in lsts:
         act = lst[::]
         exp = sorted(lst)
-        merge_sort(act, 0, len(act)-1)
-        #print(exp, act)
+        merge_sort(act, 0, len(act) - 1)
+        # print(exp, act)
         assert exp == act
 
     item_min = -10
@@ -70,10 +66,11 @@ def test_merge_sort():
         lst = [random.randint(item_min, item_max) for _ in range(size)]
         exp = sorted(lst)
         print(lst)
-        merge_sort(lst, 0, len(lst)-1)
+        merge_sort(lst, 0, len(lst) - 1)
         print(exp, lst)
         assert exp == lst
-        
+
+
 if __name__ == "__main__":
-    # test_quicksort()
+    test_quicksort()
     test_merge_sort()
